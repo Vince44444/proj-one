895800184fe03fdbb527e040affd5f667ddcbc2c
VinayakAgarwal4444 <4vagarwal@gmail.com>
2025-06-20 21:56:35 +1000

    changed url to env var

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://my-app-backend-sjfc.onrender.com' || 'http://localhost:5000'
+const API_BASE_URL = process.env.VUE_APP_API_URL || 'http://localhost:5000'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
